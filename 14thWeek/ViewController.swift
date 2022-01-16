00caf0dc8a2f20103553121d335f1815ae91f90e
Berkay  Kaya <mberkaykaya@outlook.com>
2022-01-16 14:16:13 +0300

    add tableView

--- 14thWeek/ViewController.swift
+++ 14thWeek/ViewController.swift
@@ -6,11 +6,47 @@
 //
 
 import UIKit
+import MobilliumBuilders
+import TinyConstraints
 
 class ViewController: UIViewController {
+
+    private let tableView = UITableViewBuilder().build()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+       
+        addSubViews()
+       
     }
+    
+}
+
+extension ViewController {
+    private func addSubViews() {
+        addTableView()
+    }
+    
+    private func addTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.edgesToSuperview(usingSafeArea: true)
+  
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = UITableViewCell()
+        tableViewCell.backgroundColor = .red
+        return tableViewCell
+    }
+    
 }
